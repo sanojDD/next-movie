3a69a68780ba7d03442815a9e0828e14deb95fbd
SanojDah <sanojdahal8@gmail.com>
2025-07-05 18:24:23 +0545

    error handling

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,7 +2,10 @@ import Link from "next/link";
 import { FiThumbsUp } from "react-icons/fi";
 export default function Card({ result }) {
   return (
-    <div className="group cursor-pointer sm:hover:shadow-slate-400 sm:shadow-md rounded-lg sm:border sm:border-slate-400 sm:m-2 transition-shadow duration-200">
+    <div
+      className="group cursor-pointer transition-transform duration-300 hover:scale-110
+    sm:hover:shadow-slate-400 sm:shadow-md rounded-lg sm:border sm:border-slate-400 sm:m-2 transition-shadow duration-200"
+    >
       <Link href={`/movie/${result.id}`}>
         <img
           src={`https://image.tmdb.org/t/p/original/${
