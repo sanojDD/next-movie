59456726dee5f5c449bde9a1c48b75fa703ba7a5
SanojDah <sanojdahal8@gmail.com>
2025-07-05 12:42:38 +0545

    integrated authentication using clerk

---
+++ src/app/sign-in/[[...sign-in]]/page.jsx
@@ -0,0 +1,10 @@
+// login
+import { SignIn } from "@clerk/nextjs";
+
+export default function Page() {
+  return (
+    <div className="flex items-center justify-center p-4">
+      <SignIn />
+    </div>
+  );
+}
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,17 @@
 import Link from "next/link";
 
 import DarkModeSwitch from "./DarkModeSwitch";
-// import { SignedIn, SignedOut, UserButton } from '@clerk/nextjs';
+import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 export default function Header() {
   return (
     <div className="flex justify-between items-center p-3 max-w-6xl mx-auto">
       <ul className="flex gap-4">
-        {/* <SignedIn>
+        <SignedIn>
           <UserButton />
         </SignedIn>
         <SignedOut>
-          <Link href={'/sign-in'}>Sign in</Link>
-        </SignedOut> */}
+          <Link href={"/sign-in"}>Sign in</Link>
+        </SignedOut>
         <li className="hidden sm:block">
           <Link href={"/"}>Home</Link>
         </li>
